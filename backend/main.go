8c91db524d23f6a45b08c9fa4f271e81ecfc4c69
carlos-wylliam <carloswylliam023@gmail.com>
2026-08-14 19:44:33 -0300

    feat: cors adiciona e configura suporte a CORS na main

--- backend/main.go
+++ backend/main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"korpteste-backend/controllers"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"http://localhost:4200"},
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders: []string{"Origin", "Content-Type"},
+	}))
 
 	r.POST("/produtos", controllers.CriarProduto)
 	r.GET("/produtos", controllers.ListarProdutos)
