8c6b057526559a5b8818ab31ac253c6925417471
carlos-wylliam <carloswylliam023@gmail.com>
2026-08-15 18:05:04 -0300

    feat: criado rota home, estilos, typescript

--- frontend/korpteste-frontend/src/app/app.routes.ts
+++ frontend/korpteste-frontend/src/app/app.routes.ts
@@ -3,8 +3,10 @@ import { ProdutosForm } from './features/produtos/produtos-form/produtos-form';
 import { ProdutosList } from './features/produtos/produto-list/produto-list';
 import { NotasForm } from './features/notas/notas-form/notas-form';
 import { NotasList } from './features/notas/notas-list/notas-list';
+import { Home } from './features/home/home';
 
 export const routes: Routes = [
+    { path: '', component: Home },
     { path: 'produtos/novo', component: ProdutosForm },
     { path: 'produtos', component: ProdutosList },
     { path: 'notas/novo', component: NotasForm },
